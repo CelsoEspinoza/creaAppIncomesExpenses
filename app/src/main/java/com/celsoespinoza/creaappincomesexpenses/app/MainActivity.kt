34a9e8cd6930cc4315192fc75ff96ffeb83f4c6f
CelsoEspinoza <c.espinoza.arce@outlook.com>
2020-09-13 18:48:20 -0500

    Implementando data binding en MainActivity

--- app/src/main/java/com/celsoespinoza/creaappincomesexpenses/app/MainActivity.kt
+++ app/src/main/java/com/celsoespinoza/creaappincomesexpenses/app/MainActivity.kt
@@ -2,12 +2,18 @@ package com.celsoespinoza.creaappincomesexpenses.app
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import com.celsoespinoza.creaappincomesexpenses.R
+import com.celsoespinoza.creaappincomesexpenses.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.helloWorld = "Hola Mundo"
     }
 }
